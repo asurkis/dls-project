9eee16962fe477d68b9490ba0359bb0f6b0465e4
Anton Surkis <antonsurkis@gmail.com>
2021-02-06 19:58:33 +0300

    first commit

---
+++ bin/dataset_download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd src/dataset
+rm -rf facades
+wget https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz
+gunzip facades.tar.gz
+tar -xf facades.tar
+rm facades.tar
